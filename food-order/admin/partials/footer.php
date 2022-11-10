d5dfa6725d9f42e054439e896ae6ef5994681b4a
samman13 <95701346+samman13@users.noreply.github.com>
2022-11-10 21:36:24 +0545

    Add files via upload

---
+++ food-order/admin/partials/footer.php
@@ -0,0 +1,16 @@
+        
+</div>
+        <!-- Menu Content Section Ends -->
+
+        <!-- Footer Section Starts -->
+        <div class="footer">
+        <div class="wrapper">
+           <p class="text-center">FoodiiLand Resturant. Developed By - <a href='#'>  Team PSS</p>
+      
+       </div>
+</div>
+        <!-- Footer Section Ends -->
+
+        
+</body>   
+</html>
---
+++ food-order/contact.php
@@ -0,0 +1,26 @@
+<?php include('partials-front/menu.php'); ?>
+
+
+
+
+<body bgcolor="#ced4c9" text="red">
+
+              <center> 
+               <fieldset style="width:200px; height:230px;background-color:#F3F6FB">
+      
+          <h3 >Let's get in touch</h3><br><br>
+          <p>
+            Mobile no : 9847988669 <br>
+          
+
+          
+              
+              Email : foodiland@gmail.com <br>
+           
+              Telephon no : 082560115<br>
+              eSewa ID : 9847988669<br>
+              Address : Koteshwor, Kathmandu</p>
+              </fieldset>
+              </center>
+
+<?php include('partials-front/footer.php'); ?>
